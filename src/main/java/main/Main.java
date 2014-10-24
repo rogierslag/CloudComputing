3da3eb92469cb11bed3317a2055e5cadae1d5279
Steffan Norberhuis <steffan@norberhuis.nl>
2014-10-24 13:37:29 +0200

    Refactor properties and credentials

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,63 @@
+package main;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import lombok.extern.slf4j.Slf4j;
+import scheduler.Scheduler;
+import amazon.Credentials;
+
+@Slf4j
+public class Main {
+
+	public Properties properties;
+	public Credentials credentials;
+
+	public Main() {
+		// Read in the properties.
+		properties = readProperties();
+		// Create the credential file.
+		credentials = new Credentials(properties);
+	}
+
+	public void start() {
+		// Create the Scheduler.
+		new Scheduler(credentials, properties);
+		// Create the workload generator.
+	}
+
+	/**
+	 * Reads the properties file and load them into a Properties object
+	 * 
+	 * @return the created Properties object
+	 */
+	private static Properties readProperties() {
+		try {
+			FileInputStream propertiesFile = new FileInputStream("scheduler.properties");
+			Properties props = new Properties();
+			props.load(propertiesFile);
+			log.info("Working with the following properties: {}", props);
+			return props;
+		} catch (FileNotFoundException e) {
+			// A problem was found, so returning to sane defaults happens in the
+			// app
+			log.error("Triggered an FileNotFound error when trying to read the properties file: {}", e);
+			return new Properties();
+		} catch (IOException e) {
+			log.error("Triggered an IO error when trying to read the properties file: {}", e);
+			return new Properties();
+		}
+	}
+
+	/**
+	 * Starts the scheduler and workload generator.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Main main = new Main();
+		main.start();
+	}
+}
--- src/main/java/scheduler/Scheduler.java
+++ src/main/java/scheduler/Scheduler.java
@@ -1,8 +1,5 @@
 package scheduler;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -11,14 +8,17 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.ObjectListing;
-import com.amazonaws.services.s3.model.S3ObjectSummary;
 import lombok.Getter;
 import lombok.Synchronized;
 import lombok.extern.slf4j.Slf4j;
+
 import org.joda.time.DateTime;
+
+import amazon.Credentials;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 
 /**
  * Created by Rogier on 17-10-14.
@@ -36,67 +36,33 @@ public class Scheduler {
 
 	/**
 	 * Create the scheduler.
-	 *
-	 * Reads the properties, starts and S3 client and starts scheduling the poll tasks
+	 * 
+	 * Reads the properties, starts and S3 client and starts scheduling the poll
+	 * tasks
 	 */
-	public Scheduler() {
-		properties = readProperties();
-		log.info("Working with the following properties: {}", properties);
-		final String awsAccessKey = properties.getProperty("aws.s3.access_key", null);
-		final String awsSecretKey = properties.getProperty("aws.s3.secret_key", null);
+	public Scheduler(Credentials awsCredentials, Properties properties) {
+		this.properties = properties;
 		int checkInterval = 15;
 		try {
-			checkInterval = Integer.parseInt(properties.getProperty("scheduler.check_every_x_seconds", "15"));
-		}
-		catch (NumberFormatException e) {
+			checkInterval = Integer.parseInt(this.properties.getProperty("scheduler.check_every_x_seconds", "15"));
+		} catch (NumberFormatException e) {
 			log.warn("Could not read properties value '{}'", "scheduler.check_every_x_seconds");
 		}
 
-		AWSCredentials awsCredentials = new AWSCredentials() {
-			@Override
-			public String getAWSAccessKeyId() {
-				return awsAccessKey;
-			}
-
-			@Override
-			public String getAWSSecretKey() {
-				return awsSecretKey;
-			}
-		};
 		s3Client = new AmazonS3Client(awsCredentials);
 
 		log.info("Start polling buckets in {} seconds and redo every {} seconds", checkInterval, checkInterval);
 
-		// Every task will start another one in succession. This ensures in case of a high load not all tasks are
+		// Every task will start another one in succession. This ensures in case
+		// of a high load not all tasks are
 		// interfering and makes it easier to reason about stuff and things
 		executorService.schedule(this.checkForTasks(checkInterval), checkInterval, TimeUnit.SECONDS);
 	}
 
 	/**
-	 * Reads the properties file and load them into a Properties object
-	 * @return the created Properties object
-	 */
-	private Properties readProperties() {
-		try {
-			FileInputStream propertiesFile = new FileInputStream("scheduler.properties");
-			Properties props = new Properties();
-			props.load(propertiesFile);
-			return props;
-		}
-		catch (FileNotFoundException e) {
-			// A problem was found, so returning to sane defaults happens in the app
-			log.error("Triggered an FileNotFound error when trying to read the properties file: {}", e);
-			return new Properties();
-		}
-		catch (IOException e) {
-			log.error("Triggered an IO error when trying to read the properties file: {}", e);
-			return new Properties();
-		}
-	}
-
-	/**
-	 *
-	 * @param rescheduleInterval how many seconds after finishing the next poll should be done
+	 * 
+	 * @param rescheduleInterval
+	 *            how many seconds after finishing the next poll should be done
 	 * @return A Runnable object for the executorservice
 	 */
 	@Synchronized
@@ -108,10 +74,10 @@ public class Scheduler {
 			public void run() {
 				String inputBucket = properties.getProperty("aws.s3.input", "input");
 				String outputBucket = "output";
-				if ( properties.containsKey("aws.s3.output")) {
+				if (properties.containsKey("aws.s3.output")) {
 					outputBucket = properties.getProperty("aws.s3.output", "output");
 				} else {
-					log.warn("Output bucket was not defined in properties file: {}",properties);
+					log.warn("Output bucket was not defined in properties file: {}", properties);
 				}
 				log.info("About to do a run of the input bucket: {}", inputBucket);
 
@@ -119,14 +85,17 @@ public class Scheduler {
 				ObjectListing list = s3Client.listObjects(inputBucket);
 				for (S3ObjectSummary object : list.getObjectSummaries()) {
 					Task tmp = new Task(object.getKey());
-					// The equals only checks on the `inputFile` so we can safely do this
+					// The equals only checks on the `inputFile` so we can
+					// safely do this
 					if (!taskQueue.contains(tmp)) {
-						// If we do not yet have the task, add the other data and schedule it
+						// If we do not yet have the task, add the other data
+						// and schedule it
 						tmp.setCreated_at(DateTime.now());
 						tmp.setOutputBucket(outputBucket);
 						tmp.setOutputFile(UUID.randomUUID() + ".some_extension");
 						tmp.setStatus(Task.Status.QUEUED);
-						// Should also assign to a node, but will do that later (separate method etc)
+						// Should also assign to a node, but will do that later
+						// (separate method etc)
 						taskQueue.add(tmp);
 						log.info("Added task to scheduler queue: {}", tmp);
 					} else {
@@ -151,10 +120,6 @@ public class Scheduler {
 	public void removeTaskFromQueue(Task task) {
 		log.info("Done with task: {}", task);
 		taskQueue.remove(task);
-	}
-
-	public static void main(String[] args) {
-		new Scheduler();
 	}
 
 }
